7851b00c44c6edc71a15b23a60ed29d0d3adc457
Mohan_007 <mohanbattula732@gmail.com>
2025-09-22 22:14:13 +0530

    Update ContactsApplication.java

--- contacts/src/main/java/com/mohan/contact/ContactsApplication.java
+++ contacts/src/main/java/com/mohan/contact/ContactsApplication.java
@@ -2,11 +2,12 @@ package com.mohan.contact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class ContactsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  extends SpringBootServletInitializer
+ {
 		SpringApplication.run(ContactsApplication.class, args);
 	}
 
